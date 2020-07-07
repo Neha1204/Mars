e568c44203647aef3302506f73a519f3878b1180
Neha Singhal <nehasinghal076@gmail.com>
2020-07-08 01:56:10 +0530

    Two to one dest walkable

--- src/finders/BreadthFirstFinder.js
+++ src/finders/BreadthFirstFinder.js
@@ -73,8 +73,6 @@ BreadthFirstFinder.prototype.findPath = function(startX, startY, endX, endY, gri
     }
     
     // fail to find the path
-    console.log("Path not found");
-    window.alert("No possible path from start to end point");
     return [];
 };
 
