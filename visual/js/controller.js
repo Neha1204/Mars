4d77e4c92d912146f84e490fbbf710c47d96bbf2
Neha Singhal <nehasinghal076@gmail.com>
2020-07-07 12:28:26 +0530

    Added draggingEnd2

--- visual/js/controller.js
+++ visual/js/controller.js
@@ -74,6 +74,11 @@ var Controller = StateMachine.create({
             to:   'draggingEnd'
         },
         {
+            name: 'dragEnd2',
+            from: ['ready', 'finished'],
+            to:   'draggingEnd2'
+        },
+        {
             name: 'drawWall',
             from: ['ready', 'finished'],
             to:   'drawingWall'
@@ -420,6 +425,10 @@ $.extend(Controller, {
             this.dragEnd();
             return;
         }
+        if (this.can('dragEnd2') && this.isEndPos2(gridX, gridY)) {
+            this.dragEnd2();
+            return;
+        }
         if (this.can('drawWall') && grid.isWalkableAt(gridX, gridY)) {
             this.drawWall(gridX, gridY);
             return;
@@ -447,6 +456,11 @@ $.extend(Controller, {
         case 'draggingEnd':
             if (grid.isWalkableAt(gridX, gridY)) {
                 this.setEndPos(gridX, gridY);
+            }
+            break;
+        case 'draggingEnd2':
+            if (grid.isWalkableAt(gridX, gridY)) {
+                this.setEndPos2(gridX, gridY);
             }
             break;
         case 'drawingWall':
